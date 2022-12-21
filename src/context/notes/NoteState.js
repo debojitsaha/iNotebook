584cbb63857805753f62dda3a08482ec00c0cc99
debojit <saha05debojit@gmail.com>
2022-12-21 13:36:51 +0530

    backend hosted on cyclic.sh

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const host = "http://localhost:5000";
+  const host = "https://drab-plum-rabbit-gown.cyclic.app";
   const noteInitial = [];
 
   const [notes, setNotes] = useState(noteInitial);
